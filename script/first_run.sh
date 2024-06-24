9fddcc251b5e8642cb1935cd931f573fc0c5f438
xiangjun-zeng <35248185+xiangjun-zeng@users.noreply.github.com>
2024-06-24 15:56:15 +0800

    Update first_run.sh

--- script/first_run.sh
+++ script/first_run.sh
@@ -34,6 +34,9 @@ sudo rm -f /mnt/swapfile
 
 
 git clone --depth=1 https://chromium.googlesource.com/chromium/tools/depot_tools.git
+cd depot_tools
+git checkout 39bc04eb9f4fbbd05ae68894cc7e1fdbbe17484e
+cd ..
 export DEPOT_TOOLS_DIR=$PWD/depot_tools
 export PATH=$DEPOT_TOOLS_DIR:$DEPOT_TOOLS_DIR/python2_bin:$DEPOT_TOOLS_DIR/python_bin:$PATH
 
