57ecc62cfba78918392eb7a1c627752ca5805e99
xiangjun-zeng <35248185+xiangjun-zeng@users.noreply.github.com>
2024-06-24 16:46:48 +0800

    Update download_depot.sh

--- script/download_depot.sh
+++ script/download_depot.sh
@@ -3,6 +3,7 @@ git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git
 export DEPOT_TOOLS_DIR=$PWD/depot_tools
 cd $DEPOT_TOOLS_DIR
 git checkout 39bc04eb
+sed -i '/goma_ctl/s/^/#&/' update_depot_tools
 cd ..
 export PATH=$DEPOT_TOOLS_DIR:$DEPOT_TOOLS_DIR/python2_bin:$DEPOT_TOOLS_DIR/python_bin:$PATH
 echo "$DEPOT_TOOLS_DIR" >> $GITHUB_PATH
