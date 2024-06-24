721c77ac1ccd99184df393f5d191248a4d94be17
xiangjun-zeng <35248185+xiangjun-zeng@users.noreply.github.com>
2024-06-24 16:03:39 +0800

    Update download_depot.sh

--- script/download_depot.sh
+++ script/download_depot.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 git clone --depth=1 https://chromium.googlesource.com/chromium/tools/depot_tools.git
 cd depot_tools
-git checkout 39bc04eb9f4fbbd05ae68894cc7e1fdbbe17484e
+git checkout 39bc04eb9
 cd ..
 export DEPOT_TOOLS_DIR=$PWD/depot_tools
 export PATH=$DEPOT_TOOLS_DIR:$DEPOT_TOOLS_DIR/python2_bin:$DEPOT_TOOLS_DIR/python_bin:$PATH
