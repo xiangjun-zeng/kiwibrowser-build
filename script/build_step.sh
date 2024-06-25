c78e8815035e5c3457643dba23c41b09a65ff658
xiangjun-zeng <35248185+xiangjun-zeng@users.noreply.github.com>
2024-06-25 18:12:30 +0800

    Update build_step.sh

--- script/build_step.sh
+++ script/build_step.sh
@@ -7,6 +7,8 @@ then
 fi
 test -f "$FLAG_STOP" && echo skip $task && exit 0 || echo ninja $task
 cd "$ROOT/src"
+export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
+echo "export JAVA_HOME=$JAVA_HOME" >> $GITHUB_ENV
 ninja -C out/arm64/ $task || ( test $? == 143 && echo ninja canceld )
 if test -n "$PUSH_TOKEN"
 then
